0032eced7e3062c8f310f7418b799c30b60bf570
actor168 <actor168@gmail.com>
2020-02-06 18:13:00 +0800

    add

---
+++ Problem468.java
@@ -0,0 +1,21 @@
+import java.util.regex.Pattern;
+
+class Problem468 {
+	public static void main(String[] args) {
+		String IP = "1.2.2.3";
+		System.out.println(new Problem468().validIPAddress(IP));
+	}
+
+	String chunkIPv4 = "([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])";
+	Pattern pattenIPv4 =
+	Pattern.compile("^(" + chunkIPv4 + "\\.){3}" + chunkIPv4 + "$");
+
+	String chunkIPv6 = "([0-9a-fA-F]{1,4})";
+	Pattern pattenIPv6 =
+	Pattern.compile("^(" + chunkIPv6 + "\\:){7}" + chunkIPv6 + "$");
+
+	public String validIPAddress(String IP) {
+		if (pattenIPv4.matcher(IP).matches()) return "IPv4";
+			return (pattenIPv6.matcher(IP).matches()) ? "IPv6" : "Neither";
+	}
+}
