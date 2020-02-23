1a5e95f818a16564aeef4a246b563ebc5d1653a7
actor168 <actor168@gmail.com>
2020-02-23 17:31:53 +0800

    add

---
+++ Problem94.java
@@ -0,0 +1,24 @@
+class Problem94 {
+
+	List<TreeNode> res = new ArrayList<>();
+
+	public static void main(String[] args) {
+		
+	}
+
+	public List<Integer> inorderTraversal(TreeNode root) {
+    	List < Integer > res = new ArrayList < > ();
+        Stack < TreeNode > stack = new Stack < > ();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+        return res;
+    }
+}
